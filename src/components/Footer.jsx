59bfba3aa389b970ac0d02879838e76b092e6ce4
AnjanaPrabhath <prabhathxdissanayake@gmail.com>
2024-02-14 22:49:51 +0530

    settled  few text issues

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='bg-gray-900 text-white flex flex-col justify-center w-full h-[50px] items-center border-t-2 border-t-gray-800'>
-        <p className='font-light text-xs'>all right received | developed by @AnjanaPrabhath 2024</p>
+        <p className='font-light text-xs'>all right received | developed and maintained by @AnjanaPrabhath 2024</p>
     </div>
   )
 }
